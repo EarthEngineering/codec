5d416909c5f5a79fa1d84752609557bcd3c4c70a
Gabriel Cardona <cgcardona@gmail.com>
2019-12-01 12:56:09 -0800

    better panic.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
 use codec::{Address, Network, Scheme};
 
 fn main() {
+    better_panic::install();
+
     // Decode base58 address
     let legacy_addr: &str = "1CM18hbqJzCnM8CaxaNQHxJcnkcYbLV5Gw";
 
-    // Decode earth address
-    let earth_address: &str = "bitcoincash:qp78r5zdgr53xszxlycksftf95wcv5a8q5szslvspl";
+    // Decode cash address
+    let cash_address: &str = "bitcoincash:qp78r5zdgr53xszxlycksftf95wcv5a8q5szslvspl";
 
-    let mut addr = Address::decode(legacy_addr).unwrap();
+    // Decode earth address
+    // let earth_address: &str = "earth:qp78r5zdgr53xszxlycksftf95wcv5a8q5szslvspl";
+
+    let mut addr: Address = Address::decode(legacy_addr).unwrap();
     println!("{:#?}", addr);
+
+    let mut cash_addr: Address = Address::decode(cash_address).unwrap();
+    println!("{:#?}", addr);
+
+    // let mut earth_addr: Address = Address::decode(earth_address).unwrap();
+    // println!("{:#?}", addr);
 
     // Change the base58 address to a test network earth address
     addr.network = Network::Main;
     addr.scheme = Scheme::CashAddr;
 
-    // Encode earth address
-    let earth_address: String = addr.encode().unwrap();
+    // Encode cash address
+    let cash_address: String = addr.encode().unwrap();
 
-    println!("{:#?}", earth_address);
-    // earthtest:qp78r5zdgr53xszxlycksftf95wcv5a8q5khw5038k
+    println!("{:#?}", cash_address);
+    // bitcoincash:qp78r5zdgr53xszxlycksftf95wcv5a8q5szslvspl
 }
